a94b9ef7b0ea75315c1a946e0d5a5b2c4561d09b
Ethan Favia <efavia215@live.com>
2016-08-12 15:41:35 -0400

    more work on ORM

--- config/orm.js
+++ config/orm.js
@@ -5,14 +5,36 @@ var orm ={
     uneatenBurgerQ : function(callback){
 
         connection.query('SELECT * FROM burgers WHERE devoured = FALSE ORDER BY date ASC', function(err, result){
-            if(err){throw err;}
+            if(err){throw err}
 
             callback(result); 
         });
     },
 
     eatenBurgerQ: function(callback){
-        
+        connection.query("SELECT * FROM burgers WHERE devoured = TRUE order by date DESC", function(err, result){
+            if (err){throw err}
+
+            callback(result);
+        })
+    },
+
+    addBurger: function(burg, callback){
+        connection.query("INSERT INTO burgers VALUES (null, ?, FALSE, CURRRENT_TIMESTAMP)", [burg], function(err, result){
+            if(err){throw err}
+
+            callback();
+        })
+    }, 
+
+    consumerBurger: function(burg, callback){
+        connection.query("UPDATE burgers SET devoured=TRUE WHERE burger_name=?", [burg], function(err, result){
+            if(err){throw err}
+
+            callback();
+        })
     }
 
 }
+
+module.exports = orm;
