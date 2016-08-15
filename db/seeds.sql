6761e75f6f1045bdec28fcfd9c122569b5a6a23f
Ethan Favia <efavia215@live.com>
2016-08-15 14:10:17 -0400

    seeds work

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO burgers
+    VALUES 
+        (null, "double che", FALSE, CURRENT_TIMESTAMP),
+        (null, "single che extra che", FALSE, CURRENT_TIMESTAMP),
+        (null, "triple che with a little less che", FALSE, CURRENT_TIMESTAMP),
+        (null, "double with no che", FALSE, CURRENT_TIMESTAMP);
+        
