6a6bcf7a4b70a6ac47ad56a5eca383e376bbae79
Ethan Favia <efavia215@live.com>
2016-08-14 22:32:31 -0400

    cleaning the redux

---
+++ config/connection.js
@@ -0,0 +1,22 @@
+var mysql = require('mysql');
+
+var contions = {
+    localsql : {
+        port: 3000,
+        host: 'localhost',
+        database: 'burgers_db',
+        user: 'root',
+        password: ''
+    },
+    heroku: {
+        port:3306,
+
+    }
+}
+
+var connection = mysql.createConnection(source.heroku);
+
+connection.connect(function(err){
+    if(err){throw err};
+    return; 
+})
